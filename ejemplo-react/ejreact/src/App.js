edf2d4dbaab9a6131959b6258973b3962e1a3ec4
Enrique Urra <enr.urra@profesor.duoc.cl>
2025-10-25 21:28:16 -0300

    Control funcionando

--- ejemplo-react/ejreact/src/App.js
+++ ejemplo-react/ejreact/src/App.js
@@ -1,8 +1,28 @@
 import logo from './logo.svg';
 import './App.css';
 import ControlNumero from './componentes/ControlNumero';
+import { useState } from 'react';
 
 function App() {
+  const [combinacion, setCombinacion] = useState([0, 0, 0]);
+  const [contrasenia, setContrasenia] = useState([1, 2, 3]);
+
+  function generarAvisoCambioNumero(indice) {
+    return (valorNuevo) => {
+      setCombinacion(combinacion.map((c, i) => {
+        if (i === indice) {
+          return valorNuevo;
+        } else {
+          return c;
+        }
+      }));
+    }
+  }
+
+  const contraseniaCoindice = contrasenia.every(
+    (v, i) => v === combinacion[i]
+  );
+
   return (
     <div>
       <h1>Descifra la contraseña!</h1>
@@ -12,10 +32,17 @@ function App() {
           </div>
           <div class="col-9 text-start">  
             <div className="d-flex">
-              <ControlNumero/>
-              <ControlNumero/>
-              <ControlNumero/>
-            </div>            
+              <ControlNumero actualizarPadre={generarAvisoCambioNumero(0)}/>
+              <ControlNumero actualizarPadre={generarAvisoCambioNumero(1)}/>
+              <ControlNumero actualizarPadre={generarAvisoCambioNumero(2)}/>
+            </div> 
+            <div class="row d-flex p-1">
+              {contraseniaCoindice ? (
+                <b>Contraseña encontrada!</b>
+              ) : (
+                <b>Esa no es la contraseña!</b>
+              )}              
+            </div>           
           </div>
       </div>  
     </div>
--- ejemplo-react/ejreact/src/componentes/ControlNumero.js
+++ ejemplo-react/ejreact/src/componentes/ControlNumero.js
@@ -1,18 +1,42 @@
 import { useState } from 'react';
 
-export default function ControlNumero() {
+export default function ControlNumero({actualizarPadre}) {
     const [numero, setNumero] = useState(0);
+
+    function aumentarNumero() {
+        let nuevo = numero;
+
+        if(nuevo == 9)
+            nuevo = 0;            
+        else
+            nuevo = nuevo + 1;
+
+        setNumero(nuevo);
+        actualizarPadre(nuevo);
+    }
+
+    function disminuirNumero() {
+        let nuevo = numero;
+
+        if(nuevo == 0)
+            nuevo = 9;            
+        else
+            nuevo = nuevo - 1;
+
+        setNumero(nuevo);
+        actualizarPadre(nuevo);
+    }
 
     return (
         <div className="col-2">
             <div className="row">
-                <div class="col-2"><button class="botonCambio">-</button></div>
+                <div class="col-2"><button onClick={disminuirNumero} class="botonCambio">-</button></div>
             </div>
             <div className="row">
                 <div class="col-2"><div class="numero">{numero}</div></div>
             </div>
             <div className="row">
-                <div class="col-2"><button class="botonCambio">+</button></div>
+                <div class="col-2"><button onClick={aumentarNumero} class="botonCambio">+</button></div>
             </div>
         </div>
     );
